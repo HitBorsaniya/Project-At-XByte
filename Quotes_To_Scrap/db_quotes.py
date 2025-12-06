f9c50be904ab2e97645104bb8b6f6a6f1ae57a5c
Hit Borsaniya <hitborasaniya@gmail.com>
2025-12-06 12:02:25 +0530

    Update db_quotes.py

--- Quotes_To_Scrap/db_quotes.py
+++ Quotes_To_Scrap/db_quotes.py
@@ -4,5 +4,5 @@ Client = MongoClient("mongodb://localhost:27017/")
 
 db = Client["Quotes"]
 
-Quotes_pl_table = db["Quotes_pl_table"]
-Quotes_pdp_table = db["Quotes_pdp_table"]
+Quotes_pl_table = db["Quotes_pl_table"] #storing the data of quotes list page with pagination 
+Quotes_pdp_table = db["Quotes_pdp_table"] #storing data of every author details page 
